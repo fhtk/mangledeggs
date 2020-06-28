c6bcacd2e8593913990319c505e78eda85296539
Alexander Nicholi <me@nicholatian.com>
2020-06-28 04:42:41 -0400

    reorganise Makefiles to build CLI and library separately

--- Makefile
+++ Makefile
@@ -5,44 +5,12 @@
 ##                       Released under BSD-2-Clause.                       ##
 ##############################################################################
 
-include etc/base.mk
+.PHONY: default cli lib
 
-PROJECT := mangledeggs
+default: cli
 
-# put a ‘1’ for the desired target types to compile
-SOFILE := 1
-AFILE  := 1
+cli: lib
+	$(MAKE) -f etc/cli.mk -C etc
 
-# space-separated path list for #includes
-# <system> includes
-INCLUDES := include
-# "local" includes
-INCLUDEL := src
-
-# space-separated library name list
-LIBS      :=
-LIBDIRS   :=
-
-# frameworks (macOS target only)
-FWORKS :=
-
-# ‘3P’ are in-tree 3rd-party dependencies
-# 3PLIBDIR is the base directory
-# 3PLIBS is the folder names in the base directory for each library
-3PLIBDIR := 3rdparty
-3PLIBS   :=
-
-# sources
-CFILES   :=
-HFILES   :=
-CPPFILES :=
-HPPFILES :=
-
-# test suite sources
-TES_CFILES   :=
-TES_HFILES   :=
-TES_CPPFILES :=
-TES_HPPFILES :=
-
-# this defines all our usual targets
-include etc/targets.mk
+lib:
+	$(MAKE) -f etc/lib.mk -C etc
---
+++ etc/lib.mk
@@ -0,0 +1,51 @@
+##############################################################################
+##                              mangledeggslib                              ##
+##                                                                          ##
+##                    Copyright © 2020 Alexander Nicholi                    ##
+##                       Released under BSD-2-Clause.                       ##
+##############################################################################
+
+include base.mk
+
+PROJECT := mangledeggs
+
+# put a ‘1’ for the desired target types to compile
+SOFILE := 1
+AFILE  := 1
+
+# space-separated path list for #includes
+# <system> includes
+INCLUDES := ../include
+# "local" includes
+INCLUDEL := ../src
+
+# space-separated library name list
+LIBS      :=
+LIBDIRS   :=
+
+# frameworks (macOS target only)
+FWORKS :=
+
+# ‘3P’ are in-tree 3rd-party dependencies
+# 3PLIBDIR is the base directory
+# 3PLIBS is the folder names in the base directory for each library
+3PLIBDIR := 3rdparty
+3PLIBS   :=
+
+# sources
+CFILES   := \
+	../src/demangle.c \
+	../src/mangle.c
+HFILES   := \
+	../include/mangledeggs.h
+CPPFILES :=
+HPPFILES :=
+
+# test suite sources
+TES_CFILES   :=
+TES_HFILES   :=
+TES_CPPFILES :=
+TES_HPPFILES :=
+
+# this defines all our usual targets
+include targets.mk
