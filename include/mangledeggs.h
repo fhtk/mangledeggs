324aa3febef0462c95f8711010722bafc39f3537
Alexander Nicholi <me@nicholatian.com>
2020-06-28 04:43:32 -0400

    yay public API! 0.x for now

---
+++ include/mangledeggs.h
@@ -0,0 +1,13 @@
+/****************************************************************************\
+ *                              mangledeggslib                              *
+ *                                                                          *
+ *                    Copyright © 2020 Alexander Nicholi                    *
+ *                       Released under BSD-2-Clause.                       *
+\****************************************************************************/
+
+#ifndef INC_API__MANGLEDEGGS_H
+#define INC_API__MANGLEDEGGS_H
+
+
+
+#endif /* INC_API__MANGLEDEGGS_H */
