a7c692318c99a4851e59cec07fb7dc8cc03c947f
Alexander Nicholi <me@nicholatian.com>
2020-07-04 04:56:27 -0400

    add public API

--- include/mangledeggs.h
+++ include/mangledeggs.h
@@ -8,6 +8,7 @@
 #ifndef INC_API__MANGLEDEGGS_H
 #define INC_API__MANGLEDEGGS_H
 
-
+void eg_mangle( const char**, const char*, const char** );
+void eg_demangle( const char*, const char***, const char** );
 
 #endif /* INC_API__MANGLEDEGGS_H */
